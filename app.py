a4ab869f26be53a006cd9fb3bef76cb82f6059e4
bhaRat <11raavan@gmail.com>
2021-06-03 11:46:04 +0530

    python Dynamic content added

--- app.py
+++ app.py
@@ -7,83 +7,32 @@ import sys
 import os
 from requests import get, post
 import urllib
+from azure.core.exceptions import ResourceNotFoundError
+from azure.ai.formrecognizer import FormRecognizerClient
+from azure.ai.formrecognizer import FormTrainingClient
+from azure.core.credentials import AzureKeyCredential
 app = Flask(__name__)
-def runAnalysis(input_file='artdoc.pdf', output_file='', file_type='application/json'):
-    # Endpoint URL
-    endpoint = r"https://fsiformrecognizer.cognitiveservices.azure.com/"
-    # Subscription Key
-    apim_key = "b1786032cc014d0f9c4cd24530e3fb6d"
-    # Model ID
-    model_id = "ca9bb8ab-a0ff-42a5-99e8-6ba5efe2f1e8"
-    # API version
-    API_version = "v2.1"
+endpoint = "https://fsiformrecognizer.cognitiveservices.azure.com/"
+key = "b1786032cc014d0f9c4cd24530e3fb6d"
+form_recognizer_client = FormRecognizerClient(endpoint, AzureKeyCredential(key))
+form_training_client = FormTrainingClient(endpoint, AzureKeyCredential(key))
+formUrl="https://fsi2.blob.core.windows.net/trainimages/FSI%20-%20DENSA%20SHARK%20-%2020.07.2020.pdf?sp=r&st=2021-05-15T07:46:30Z&se=2022-05-15T15:46:30Z&spr=https&sv=2020-02-10&sr=b&sig=j6Hfe9DeLN5eaLjMxDcMu6l8kQzciglHRcm4NOlUswI%3D"
+poller = form_recognizer_client.begin_recognize_custom_forms_from_url(model_id="ca9bb8ab-a0ff-42a5-99e8-6ba5efe2f1e8", form_url=formUrl)
+result = poller.result()
+tags=[]
+for recognized_form in result:
+    print("Form type: {}".format(recognized_form.form_type))
+    for name, field in recognized_form.fields.items():
+        print("Field '{}' has value '{}' and a confidence score of {}".format(
+            name,
+            field.value,
+            field.confidence
+        ))
+        tags.append(name)
 
-    post_url = endpoint + "/formrecognizer/%s/custom/models/%s/analyze" % (API_version, model_id)
-    params = {
-        "includeTextDetails": True
-    }
 
-    headers = {
-        # Request headers
-        'Content-Type': file_type,
-        'Ocp-Apim-Subscription-Key': apim_key,
-    }
-    try:
-        filej = urllib.request.Request('https://fsi2.blob.core.windows.net/trainimages/newtestdensa.pdf')
-        with urllib.request.urlopen(filej) as responsee:
-            data_bytes = responsee.read()
-    except IOError:
-        print("Inputfile not accessible.")
-        sys.exit(2)
-
-    try:
-        print('Initiating analysis...')
-        resp = post(url = post_url, data = data_bytes, headers = headers, params = params)
-        if resp.status_code != 202:
-            print("POST analyze failed:\n%s" % json.dumps(resp.json()))
-            quit()
-        print("POST analyze succeeded:\n%s" % resp.headers)
-        print
-        get_url = resp.headers["operation-location"]
-    except Exception as e:
-        print("POST analyze failed:\n%s" % str(e))
-        quit()
-
-    n_tries = 15
-    n_try = 0
-    wait_sec = 5
-    max_wait_sec = 60
-    print()
-    print('Getting analysis results...')
-    while n_try < n_tries:
-        try:
-            resp = get(url = get_url, headers = {"Ocp-Apim-Subscription-Key": apim_key})
-            resp_json = resp.json()
-            if resp.status_code != 200:
-                print("GET analyze results failed:\n%s" % json.dumps(resp_json))
-                quit()
-            status = resp_json["status"]
-            if status == "succeeded":
-                if output_file:
-                    with open(output_file, 'w') as outfile:
-                        json.dump(resp_json, outfile, indent=2, sort_keys=True)
-                print("Analysis succeeded:\n%s" % json.dumps(resp_json["analyzeResult"]["documentResults"][0]["fields"], indent=2, sort_keys=True))
-                quit()
-            if status == "failed":
-                print("Analysis failed:\n%s" % json.dumps(resp_json))
-                quit()
-            # Analysis still running. Wait and retry.
-            time.sleep(wait_sec)
-            n_try += 1
-            wait_sec = min(2*wait_sec, max_wait_sec)     
-        except Exception as e:
-            msg = "GET analyze results failed:\n%s" % str(e)
-            print(msg)
-            quit()
-    print("Analyze operation did not complete within the allocated time.")
 
 
 @app.route("/")
 def hello():
-    runAnalysis()
-    return render_template("index.html")
+    return render_template("index.html",output=tags[0])
